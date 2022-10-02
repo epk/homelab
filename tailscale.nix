3a7e363e48dcfd5276caafce2b827d166ad480a8
Scott Bronson <brons_github@rinspin.com>
2022-10-01 18:50:32 -0700

    Use overlay instead of override

--- tailscale.nix
+++ tailscale.nix
@@ -1,20 +1,12 @@
 { config, lib, pkgs, ... }:
-
 let
-
   baseconfig = { allowUnfree = true; };
   unstable = import <nixpkgs-unstable> { config = baseconfig; };
-
 in {
-
-  imports =
-    [ <nixpkgs-unstable/nixos/modules/services/networking/tailscale.nix> ];
-  disabledModules = [ "services/networking/tailscale.nix" ];
-
-  nixpkgs.config = baseconfig // {
-    packageOverrides = pkgs: { tailscale = unstable.tailscale; };
-  };
+  nixpkgs.overlays = [(final: prev: {
+    tailscale = unstable.tailscale;
+  })];
 
   services.tailscale.enable = true;
-
 }
+
